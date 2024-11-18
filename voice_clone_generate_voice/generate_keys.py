addbb34a9fea5eeabb5e619f996506b84e4ec3d6
Leena K I <120550152+LeenaKI@users.noreply.github.com>
2024-11-18 13:40:24 +0530

    Update generate_keys.py

--- voice_clone_generate_voice/generate_keys.py
+++ voice_clone_generate_voice/generate_keys.py
@@ -1,14 +1,25 @@
-import pickle
-from pathlib import Path
-
+import streamlit as st
 import streamlit_authenticator as stauth
 
-names = ["Leena K I"]
-usernames = ["leena"]
-passwords = ["XXX"]
+def login():
+    names = ['Leena K I']
+    usernames = ['leena']
+    passwords = ['clone@20127']
+    hashed_passwords = stauth.Hasher(passwords).generate()
+    authenticator = stauth.Authenticate(names,usernames,hashed_passwords,'some_cookie_name','some_signature_key',cookie_expiry_days=30)
+    name, authentication_status, username = authenticator.login('Login', 'main')
+        
+    if st.session_state["authentication_status"]:
+        test=authenticator.logout('Logout', 'main')
+        st.write(f'Welcome *{st.session_state["name"]}*')
+        st.title('Some content')
+    elif st.session_state["authentication_status"] == False:
+        st.error('Username/password is incorrect')
+    elif st.session_state["authentication_status"] == None:
+        st.warning('Please enter your username and password')
+        
+def main():
+    login()
 
-hashed_passwords = stauth.Hasher(passwords).generate()
-
-file_path = Path(__file__).parent / "hashed_pw.pkl"
-with file_path.open("wb") as file:
-    pickle.dump(hashed_passwords, file)
+if __name__ == "__main__":
+    main()
