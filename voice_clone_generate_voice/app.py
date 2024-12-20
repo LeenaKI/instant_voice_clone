46d2793a361c70e0e7132095757d0bf09aae0813
Leena K I <120550152+LeenaKI@users.noreply.github.com>
2024-12-20 23:27:09 +0530

    Update app.py

--- voice_clone_generate_voice/app.py
+++ voice_clone_generate_voice/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from voice_training import show_voice_training_page
 from voice_cloning import show_voice_cloning_page
-from tts_generation import show_tts_generation_page
+#from tts_generation import show_tts_generation_page
 
 # Function to handle login
 def login():
@@ -30,7 +30,7 @@ else:
     #st.title(f"Welcome {st.session_state.username}")
     
     # Sidebar for page navigation
-    st.sidebar.title("Voice Changer App")
+    st.sidebar.title("Instant Voice Cloning App")
     page = st.sidebar.radio("Choose a page", ["Voice Training", "Voice Cloning", "Logout"])
 
     # Route to selected page
@@ -38,7 +38,7 @@ else:
         show_voice_training_page()
     elif page == "Voice Cloning":
         show_voice_cloning_page()
-    elif page == "TTS Generation":
-        show_tts_generation_page()
+    #elif page == "TTS Generation":
+        #show_tts_generation_page()
     elif page == "Logout":
         logout()
