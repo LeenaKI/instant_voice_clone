668a98136d04d48afdeb15d899748e55efb4ed6e
Leena K I <120550152+LeenaKI@users.noreply.github.com>
2024-12-20 16:13:47 +0530

    Update app.py

--- voice_clone_generate_voice/app.py
+++ voice_clone_generate_voice/app.py
@@ -8,7 +8,7 @@ def login():
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
     if st.button("Login"):
-        if username == "leena" and password == "clone@20127":
+        if username == "amiga" and password == "clone@20127":
             st.session_state.logged_in = True
             st.session_state.username = username
             st.success("Login successful!")
